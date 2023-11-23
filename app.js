b071d30e90e9b672d5573396a16828bff605bea8
Yonghua22 <yfyfjade@gmail.com>
2023-11-22 22:33:43 -0800

    fix

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import express from "express";
 import Lab5 from "./lab5.js";
-import CourseRoutes from "./Courses/routes.js";
-import ModuleRoutes from "./Modules/routes.js";
+import CourseRoutes from "./courses/routes.js";
+import ModuleRoutes from "./modules/routes.js";
 import "dotenv/config";
 import cors from "cors";
 const app = express();
