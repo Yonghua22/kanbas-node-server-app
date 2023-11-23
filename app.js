be0226184a90caba3a0b65afb013687f4778e4ac
Yonghua22 <yfyfjade@gmail.com>
2023-11-22 22:20:05 -0800

    check

--- app.js
+++ app.js
@@ -1,15 +1,31 @@
-import express from "express";
+import express from 'express';
+import Hello from "./hello.js"
 import Lab5 from "./lab5.js";
 import CourseRoutes from "./courses/routes.js";
-import ModuleRoutes from "./modules/routes.js";
 import cors from "cors";
+import ModuleRoutes from "./modules/routes.js";
+import AssignmentRoutes from './assignments/routes.js';
 import "dotenv/config";
+
+
 const app = express();
-app.use(cors());
+const allowedOrigins = ['http://localhost:3000', 'https://a5--aesthetic-smakager-441fa4.netlify.app'];
+
+app.use(cors({
+    credentials: true,
+    origin: function (origin, callback) {
+        if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    }
+}));
+
 app.use(express.json());
-ModuleRoutes(app);
 CourseRoutes(app);
+ModuleRoutes(app);
+AssignmentRoutes(app);
 Lab5(app);
-// app.listen(4000);
-app.listen(process.env.PORT || 4000);
-app.use(cors());
+Hello(app);
+app.listen(process.env.PORT ||8000);
