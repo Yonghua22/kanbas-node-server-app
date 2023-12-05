9f087a623a766bf8e4514ce1728a2ae7c3d57a14
Yonghua22 <yfyfjade@gmail.com>
2023-12-04 20:47:17 -0800

    a6 Mongo fa23

--- app.js
+++ app.js
@@ -4,9 +4,12 @@ import Lab5 from "./lab5.js";
 import CourseRoutes from "./courses/routes.js";
 import cors from "cors";
 import ModuleRoutes from "./modules/routes.js";
+import UserRoutes from "./users/routes.js";
 // import AssignmentRoutes from './assignments/routes.js';
 // import "dotenv/config";
 
+import mongoose from "mongoose";
+mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
 
 const app = express();
 const allowedOrigins = ['http://localhost:3000', 'https://a5--aesthetic-smakager-441fa4.netlify.app'];
@@ -28,5 +31,6 @@ ModuleRoutes(app);
 // AssignmentRoutes(app);
 Lab5(app);
 Hello(app);
+UserRoutes(app);
 //app.listen(process.env.PORT ||4000);
 app.listen(4000);
